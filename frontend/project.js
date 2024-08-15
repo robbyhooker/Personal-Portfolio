983d6a7e1ab687ab365be0861af8119fd3e9c28f
robbyhooker <robbyhooker29@gmail.com>
2024-08-15 17:42:27 -0400

    update image paths

--- frontend/project.js
+++ frontend/project.js
@@ -426,35 +426,35 @@ const projects = {
             <p><strong>Objective:</strong> Track the progression of cumulative wins for teams and drivers.</p>
             <p><strong>Visualization:</strong> Line plots show the accumulation of wins, highlighting periods of dominance.<br><br> We see in the cumulative team wins chart that Scuderia Ferrari have always been the top team in racing.<br>However, with recent struggles and epic runs by Red Bull and Mercedes, they have entered the conversation, along with McLaren.</p>
             <div class="charts">
-                <img src="assets/charts/cum_teams.png" alt="Cumulative Team Wins">
+                <img src="assets/Charts/cum_teams.png" alt="Cumulative Team Wins">
             </div>
             <p>Based on driver wins over time, it is hard to deny that Lewis Hamilton is the greatest racer of all time.<br>Max Verstappen's astonishing trajectory may indicate that he'll threaten this title.</p>
             <div class="charts">
-                <img src="assets/charts/cum_drivers.png" alt="Cumulative Driver Wins">
+                <img src="assets/Charts/cum_drivers.png" alt="Cumulative Driver Wins">
             </div>
 
             <h3>2. Dominance Periods</h3>
             <p><strong>Objective:</strong> Identify specific periods when teams or drivers were dominant.</p>
             <p><strong>Visualization:</strong> Time-dependent bar plot shows dominant eras for teams and drivers.<br><br> Team dominance shows how Ferrari got their lead in the wins category, but also exposes their recent absence.<br>Hopefully Leclerc and Sainz can be catalysts of change in 2025.</p>
             <div class="charts">
-                <img src="assets/charts/dom_teams.png" alt="Dominant Teams">
+                <img src="assets/Charts/dom_teams.png" alt="Dominant Teams">
             </div>
             <p>Driver dominance periods further demonstrate Verstappen and Hamilton's recent grip on the sport.</p>
             <div class="charts">
-                <img src="assets/charts/dom_drivers.png" alt="Dominant Drivers">
+                <img src="assets/Charts/dom_drivers.png" alt="Dominant Drivers">
             </div>
 
             <h3>3. Race Characteristics: Lap Times and Evolution</h3>
             <p><strong>Objective:</strong> Examine the evolution of average lap times in races.</p>
             <p><strong>Visualization:</strong> Time series of the average lap time over each year shows the trend of speed in the sport.<br><br> A running average of lap times shows improvements in race speeds over time, influenced by technological advancements and changes in regulations.<br>However, we see lap times in the 1960s became slower due to regulatory changes, including reduced engine capacities and an increased focus on safety.</p>
             <div class="charts">
-                <img src="assets/charts/lap_avg.png" alt="Average Lap Times">
+                <img src="assets/Charts/lap_avg.png" alt="Average Lap Times">
             </div>
 
             <h3>Conclusion</h3>
             <p>Scuderia Ferrari is clearly the most decorated team historically, but we need to reverse the narrative of recent years.</p>
             <div class="charts">
-                <img src="assets/charts/tifosi.jpg" alt="Tifosi">
+                <img src="assets/Charts/tifosi.jpg" alt="Tifosi">
             </div>
 
             <h3>Future Work</h3>
